2e57a131180bcd70fec3d6d2628802522b2b67f3
nicco70 <nicolasgf_98@hotmail.com>
2023-06-29 03:45:21 -0300

    commit final quiosco app

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+import { QuioscoProvider } from "../context/QuioscoProvider";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <QuioscoProvider>
+      <Component {...pageProps} />
+    </QuioscoProvider>
+  );
+}
+
